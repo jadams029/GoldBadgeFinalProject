2c2a41227c66dcc5109442b4e654e206cb1227ea
Jason Adams <adams.jason.029@gmail.com>
2021-11-15 17:45:56 -0500

    challenge one actually done

--- ChallengeOne/UI/ChalOneUI.cs
+++ ChallengeOne/UI/ChalOneUI.cs
@@ -33,21 +33,22 @@ namespace ChallengeOne.UI
                 {
                     case "1":
                     case "one":
+                        ShowFullMenu();
                         break;
                     case "2":
                     case "two":
+                        AddItemToMenu();
                         break;
                     case "3":
                     case "three":
+                        RemoveItemFromMenu();
                         break;
                     case "4":
                     case "four":
                     case "exit":
                         keepRunning = false;
                         break;
-                    default: Console.WriteLine("Please enter a valid selection above. \n"+
-                        "Press any key to continue ...");
-                        Console.ReadKey();
+                    default: AnyKey();
                         break;
                 }
             }
@@ -58,12 +59,81 @@ namespace ChallengeOne.UI
             List<KomodoMenu> menuList = _KMenu.GetMenuItems();
             foreach (KomodoMenu item in menuList)
             {
-                
+                DisplayMenuStuff(item);
+            }
+            AnyKey();
+        }
+        private void AddItemToMenu()
+        {
+            Console.Clear();
+            KomodoMenu menu = new KomodoMenu();
+            Console.WriteLine("Enter a Meal Number: ");
+            menu.MealNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a Meal Name: ");
+            menu.MealName = Console.ReadLine();
+            Console.WriteLine("Enter a Meal Description: ");
+            menu.MealDescrip = Console.ReadLine();
+            Console.WriteLine("Enter a Meal Price: ");
+            menu.MealPrice = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Meal Indgredients: ");
+            Console.WriteLine("Enter Ingredient Items:  ");
+            Console.WriteLine("1. Fries \n" +
+                "2. Drink \n" +
+                "3. Burger \n" +
+                "4. Chicken Sandwich \n" +
+                "5. Chicken Nuggets ");
+            string ingredientInput = Console.ReadLine();
+            int ingrID = int.Parse(ingredientInput);
+            menu.ItemIngredients = (IngredientsList)ingrID;
+            if (_KMenu.AddItemToMenu(menu))
+            {
+                Console.WriteLine("Menu Item Added.");
+                AnyKey();
+            }
+            else
+            {
+                Console.WriteLine("Menu Item not added");
+                AnyKey();
+            }
+            
+        }
+        private void RemoveItemFromMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("What menu item would you like to remove?\n");
+            List<KomodoMenu> currentMenu = _KMenu.GetMenuItems();
+            int count = 0;
+            foreach (KomodoMenu item in currentMenu)
+            {
+                count++;
+                Console.WriteLine($"{count}. {item.MealNumber}");
+            }
+            int targetMealNum = int.Parse(Console.ReadLine());
+            int targetIndex = targetMealNum - 1;
+            if (targetIndex >=0 && targetIndex < currentMenu.Count)
+            {
+                KomodoMenu desiredMenu = currentMenu[targetIndex];
+                Console.WriteLine($"{desiredMenu.MealNumber} was removed.");
+                AnyKey();
+            }
+            else
+            {
+                Console.WriteLine("Item not removed");
+                AnyKey();
             }
         }
         private void DisplayMenuStuff(KomodoMenu menu)
         {
-            Console.WriteLine($"");
+            Console.WriteLine(
+                $"Meal Number: {menu.MealNumber}\n"+
+                $"Meal Name: {menu.MealName}" +
+                $"Meal Descrip: {menu.MealDescrip}" +
+                $"Meal Price: {menu.MealPrice}");
+        }
+        private void AnyKey()
+        {
+            Console.WriteLine("Press any key to continue ...");
+            Console.ReadKey();
         }
 
     }
